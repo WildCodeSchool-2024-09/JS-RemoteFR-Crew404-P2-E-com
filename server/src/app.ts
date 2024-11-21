6dc7f1085ed6f657987c2d16608f6afb44bf4caf
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-11-21 10:44:02 +0100

    modify app.ts server

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,11 +20,7 @@ const app = express();
 
 import cors from "cors";
 
-if (process.env.CLIENT_URL != null) {
-  app.use(
-    cors({ origin: [process.env.CLIENT_URL || "http://localhost:3000"] }),
-  );
-}
+app.use(cors({ origin: [process.env.CLIENT_URL || "http://localhost:3000"] }));
 
 // If you need to allow extra origins, you can add something like this:
 
