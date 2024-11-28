b413bb13deacaee95747b2f1a2fabd28ec64ef1d
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-11-28 17:15:17 +0100

    fix img

--- client/src/components/Product/ProductImage.tsx
+++ client/src/components/Product/ProductImage.tsx
@@ -6,7 +6,7 @@ function ProductImage({ imageUrl, name }: ProductImageProps) {
   return (
     <div className="relative aspect-square overflow-hidden rounded-xl bg-gray-100">
       <img
-        src={`http://localhost:3310/${imageUrl}`}
+        src={`https://e-com-404.netlify.app/${imageUrl}`}
         alt={name}
         className="object-cover w-full h-full hover:scale-105 transition-transform duration-300"
       />
